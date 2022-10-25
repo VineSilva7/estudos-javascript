549f3e1425a12cfe95bb342bdd20191e184954cc
Vinicius da Silva <v_s199713@hotmail.com>
2022-10-25 13:08:31 -0300

    exercicios aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.5
+console.log(`A velocidade de seu carro é de ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log('Diriga sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log(`Voce é Brasieliro`)
+} else {
+    console.log(`Voce é Extrangeiro`)
+}
