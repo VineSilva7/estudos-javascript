cad1af73892b4d3c7e51b1ed4db8ebd5fa77d7d5
Vinicius da Silva <v_s199713@hotmail.com>
2022-10-31 13:44:48 -0300

    exercicio de hora

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,24 @@
-alert('Ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora > 0 && hora < 12) {
+        //Bom Dia
+        img.src='manha.png'
+        document.body.style.background = '#d5be8f'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa Tarde
+        img.src='tarde.png'
+        document.body.style.background = '#ee8e53'
+    } else {
+        //Boa Noite
+        img.src='noite.png'
+        document.body.style.background = '#06353a'
+    }
+    
+}
+
