ff4a2652ff7f97124234f72afe233724c1af0ceb
Vinicius da Silva <v_s199713@hotmail.com>
2022-11-03 14:07:45 -0300

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*
+//ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO FINAL
+var c = 1
+do {
+    console.log((`Passo ${c}`))
+    c++
+} while (c <= 6)
+*/
+
+
+
+/*
+//ESTRUTURA DE REPETIÇÃO COM TESTE LOGICO NO INICIO
+var c = 1
+while (c <= 6) {
+    console.log((`Passo ${c}`))
+    c++
+}
+*/
