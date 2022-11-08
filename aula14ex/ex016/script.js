b052fe7910f655b1c3a8af839a2fd6a624e585ae
Vinicius da Silva <v_s199713@hotmail.com>
2022-11-08 12:58:56 -0300

    exercicio parte 04

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,12 +6,35 @@ function contar() {
     let res = window.document.getElementById('res')
 
 
-if (inicio.valeu.length == 0 || fim.valeu.length == 0 || passo.valeu.length == 0) {
-    window.alert(`[ERRO] Faltam Dados`)
+if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = `Impossível contar!`
+    window.alert(`[ERRO] Faltam Dados!`)    
 } else {
-    if (passo == 0) {
-        window.alert(`Passo Inválido! Considerado Passo 1`)
+    res.innerHTML = `Contando: <br>`
+
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+    if (p <= 0) {
+        window.alert(`Passo Inválido! Considerarando Passo 1!`)
+        p = 1
+    }
+
+    if (i < f) {
+        //CONTAGEM CRESCENTE
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${ c } \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } else {
+        //CONTAGEM REGRESSIVA
+        for (let c = i; c >= f; c -= p) {
+            res.innerHTML += `${ c } \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}` 
+    }
+           
     }
 }
   
-}
