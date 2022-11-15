f49c22d8339cef4d064c0935a22d27242df81a8f
Vinicius da Silva <v_s199713@hotmail.com>
2022-11-15 14:39:59 -0300

    exercicios parte 7

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,28 @@
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isnumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isnumero(num.value) && !inlista(num.value, valores)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor Inválido ou já encontrado na lista!')
+    }
+}
